0b4037f619fd10c798fcba7ab84b4218195d498a
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-11-22 10:07:30 +0630

    banner and hero style

--- src/custom/customtheme.js
+++ src/custom/customtheme.js
@@ -8,6 +8,13 @@ const customtheme=()=>{
           dark:{
             main:"#424242",
             light:"#757575",
+          },
+          secondary:{
+            main:"#e3f2fd",
+            light:"#e1f5fe"
+          },
+          warning:{
+            main:"#ffca28"
           }
         }
     })
